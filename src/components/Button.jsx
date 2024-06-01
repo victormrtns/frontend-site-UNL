5ca3c2761c973e9bed4f98e7924d081f911cc88b
Victor <88071929+victormrtns@users.noreply.github.com>
2024-06-01 18:41:43 -0300

    [feat] - responsive part almost done

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Button = ({ className,children,page,type,border}) => {
   return (
     <Link to = {page}>
-      <button className = {`flex justify-center align-center leading-none w-[300px] h-[48px] px-[10px] py-[10px] text-[28px] rounded-full ${className} ${(type === 'filled')?' bg-mainPink':'bg-white'}
+      <button className = {`flex justify-center align-center leading-none w-fit h-fit px-[10px] py-[10px] text-[19px] lg:px-[30px] lg:py-[15px] lg:text-[22px] sm:px-[5px] sm:text-[23px] rounded-full ${className} ${(type === 'filled')?' bg-mainPink':'bg-white'}
       ${(border===true)?'border-2 border-[#B3B3B3]':''}`}>
         {children}
       </button>
