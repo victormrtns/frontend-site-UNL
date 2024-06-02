2e274965133ba977a297c3a920b6ce0a0d7644e6
Douglas Nicida <douglasnicida@gmail.com>
2024-06-02 04:11:55 -0300

    [feat] - Go to forms button working and localStorage for age

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
-const Button = ({ className,children,page,type,border, onClick}) => {
+const Button = ({ className,children,page,type,border,onClick}) => {
   return (
-    <Link to = {page}>
-      <button className = {`flex justify-center align-center leading-none w-fit h-fit px-[10px] py-[10px] text-[19px] lg:px-[30px] lg:py-[15px] lg:text-[22px] sm:px-[5px] sm:text-[23px] rounded-full ${className} ${(type === 'filled')?' bg-mainPink':'bg-white'}
-      ${(border===true)?'border-2 border-[#B3B3B3]':''}`} onClick={onClick}>
+    <Link to={page}>
+      <button className = {`flex justify-center align-center leading-none w-fit h-fit px-[10px] py-[10px] lg:px-[30px] lg:py-[15px] sm:px-[20px] rounded-full ${className} 
+      ${(type === 'filled')?' bg-mainPink hover:bg-[#ce6fae]':'bg-white'}
+      ${(border===true)?'border-2 border-[#B3B3B3]':''} transition-all duration-300 ease-in-out`} onClick={onClick}>
         {children}
       </button>
     </Link>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return ( 
-        <div className="w-full bg-mainPink h-12">
+        <div className="w-full bottom-0 bg-mainPink h-12">
 
         </div>
      );
